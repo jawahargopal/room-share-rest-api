7756614e4bf9f2698d5f59c607d9c733fccd73f4
Jawahar Gopal <gjawahargopal@gmail.com>
2018-07-07 20:29:06 +0530

    Generated users obj dynamically

--- api/routes/reportRoutes.js
+++ api/routes/reportRoutes.js
@@ -2,6 +2,7 @@ var express = require('express'),
                 reportRouter = express.Router(),                
                 mongoose = require('mongoose'),
                 Expense = require('../models/expenseModel'),
+                Users = require('../models/userModel');
                 Constants = require('../constants');
 reportRouter.route('/')
     .get((req, res) => {
@@ -9,26 +10,27 @@ reportRouter.route('/')
         if (err)
             res.send(err);
         var rent = Constants.ROOM_RENT/Constants.MEMBERS_COUNT;
-        var users = {
-          1:{cr:0,dr:rent},
-          2:{cr:0,dr:rent},
-          3:{cr:0,dr:rent},
-          4:{cr:0,dr:rent}
-        }
-        for(expense of expenses) {
-          for (user in users) {
-            if (expense.user_id == user) {
-              users[user].cr += expense.amount;
-            }
-            if (expense.type === 1) {              
-              users[user].dr += expense.share_amount;
-            } else {
-              if (expense.contribution.indexOf(user) > -1) {
-                users[user].dr += expense.share_amount; 
+        var users = {};
+        Users.find({}, function(err, users_data) {
+          for (user of users_data) {
+            users[user._id] = {name:user.name,cr:0,dr:rent}
+          }
+          for(expense of expenses) {
+            for (user in users) {
+              if (expense.user_id == user) {
+                users[user].cr += expense.amount;
               }
-            }
-          }   
-        }
+              if (expense.type === 1) {              
+                users[user].dr += expense.share_amount;
+              } else {
+                if (expense.contribution.indexOf(user) > -1) {
+                  users[user].dr += expense.share_amount; 
+                }
+              }
+            }   
+          }
+
+        });
         res.json(users);
       });
     });
