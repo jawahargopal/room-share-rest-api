8017371c870baafb628ebcfe668c6497927604f6
Jawahar Gopal <gjawahargopal@gmail.com>
2018-07-10 20:33:51 +0530

    removed unwanted code

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ var express = require('express'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser');
 
-
-  console.log('Hiiiii');
-  
 // mongoose instance connection url connection
-//mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/expenseManager'); 
-//mongoose.connect('mongodb://admin:spartans300@ds219641.mlab.com:19641/room-share');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/expenseManager');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,7 +15,7 @@ var entry = function (req, res, next) {
   next();  
 }
 
-/*var categoryRouter = require('./api/routes/categoryRoutes');
+var categoryRouter = require('./api/routes/categoryRoutes');
 var userRouter = require('./api/routes/userRoutes');
 var expenseRouter = require('./api/routes/expenseRoutes');
 var reportRouter = require('./api/routes/reportRoutes');
@@ -28,12 +24,7 @@ app.use(entry);
 app.use('/api/categories', categoryRouter);
 app.use('/api/users', userRouter);
 app.use('/api/expenses', expenseRouter);
-app.use('/api/reports', reportRouter);*/
-
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
-
+app.use('/api/reports', reportRouter);
 
 
 app.listen(port);
